5bd65d7b098c31c9c73d200c200c970d91fdf2ad
Joshua Benfield <josh@benfield.org>
2023-05-17 18:25:35 -0400

    added tokio - async networking implemented - multiple connection support - full command support

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env};
+use std::{env, sync::Arc};
+use tokio::{io::{self, AsyncReadExt, AsyncWriteExt}, net::TcpListener, sync::Mutex};
 
 struct Cache {
     buffer: Vec<u8>,
@@ -18,7 +19,7 @@ impl Cache {
         range.1
     }
 
-    fn set(self: &mut Self, key: String, data: &[u8]) -> Result<(usize, usize), String> {
+    fn set(self: &mut Self, key: String, data: &[u8]) -> Result<(), String> {
         let data_length = data.len();
         let mut start = self.next_index();
 
@@ -45,7 +46,7 @@ impl Cache {
 
         self.mapping.push((key, range));
 
-        Ok(range)
+        Ok(())
     }
 
     fn get(self: &Self, key: String) -> Option<&[u8]> {
@@ -77,23 +78,109 @@ impl Cache {
     }
 }
 
-fn main() {
+#[derive(Debug)]
+enum Command {
+    UNKNOWN = 255,
+    QUIT = 1,
+    GET = 2,
+    SET = 3,
+    EXISTS = 4,
+}
+
+impl Command {
+    fn from_u8(c: u8) -> Command {
+        match c {
+            1 => Command::QUIT,
+            2 => Command::GET,
+            3 => Command::SET,
+            4 => Command::EXISTS,
+            _ => Command::UNKNOWN,
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let mut allocated: usize = 0;
+    let mut allocated: usize = 1_024 * 1_024; // 1 mb
+    let mut max_value_size: usize = 1_024; // 1 kb
 
-    if args.len() < 2 {
-        allocated = 1 * 1_024 * 1_024; // 1 mb
-    } else {
-        let memory_arg = &args[1];
+    if args.len() >= 3 {
+        let alloc_arg = &args[1];
+        let value_arg = &args[2];
 
-        match memory_arg.parse::<usize>() {
+        match alloc_arg.parse::<usize>() {
             Ok(v) => allocated = v * 1_024 * 1_024,
-            Err(_) => println!("{} is not a valid memory value! must be in mb", memory_arg)
+            Err(_) => println!("{} is not a valid value for allocated memory! must be in mb. eg: 1mb", alloc_arg)
+        }
+
+        match value_arg.parse::<usize>() {
+            Ok(v) => max_value_size = v * 1_024,
+            Err(_) => println!("{} is not a valid value for the max value size! must be in kb. eg: 1kb", alloc_arg)
         }
     }
 
-    let mut cache = Cache {
+    let cache = Arc::new(Mutex::new(Cache {
         buffer: vec![0; allocated],
         mapping: vec![]
-    };
+    }));
+
+    let listener = TcpListener::bind("127.0.0.1:9055").await?;
+
+    println!("[i] listening on localhost:9055");
+
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+
+        println!("[+] {:?}", addr);
+
+        tokio::spawn({
+            let cache = cache.clone();
+
+            async move {
+                loop {
+                    let mut buf = vec![0 as u8; 1 + 32 + max_value_size];
+                    let n = socket.read(&mut buf).await?;
+
+                    if n == 0 && buf[0] == 0 {
+                        break;
+                    }
+
+                    let command = Command::from_u8(buf[0]);
+                    let key = String::from_utf8_lossy(&buf[1..33]).trim_end_matches(char::from(0)).to_string();
+                    let data = &buf[33..];
+
+                    match command {
+                        Command::QUIT => {
+                            println!("[-] {:?}", addr);
+
+                            socket.shutdown().await?;
+                        },
+                        Command::GET => {
+                            match &mut cache.lock().await.get(key) {
+                                Some(res) => socket.write_all(res).await?,
+                                None => socket.write_all(&[0]).await?
+                            }
+                        },
+                        Command::SET => {
+                            match &mut cache.lock().await.set(key, data) {
+                                Ok(_) => socket.write_all(&[1]).await?,
+                                Err(e) => socket.write_all(e.as_bytes()).await?
+                            }
+                        },
+                        Command::EXISTS => {
+                            match &mut cache.lock().await.exists(key) {
+                                Some(_) => socket.write_all(&[1]).await?,
+                                None => socket.write_all(&[0]).await?
+                            }
+                        },
+                        Command::UNKNOWN => socket.write_all(&"UNKNOWN".as_bytes()).await?,
+                    }
+                }
+
+                println!("[-] {:?}", addr);
+                Ok::<(), io::Error>(())
+            }
+        });
+    }
 }
